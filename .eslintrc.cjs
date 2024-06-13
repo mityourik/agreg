5302cc5110f4d4e56fb840fb1c3d33347fbcc207
Dmitry <mityourik@gmail.com>
2024-06-13 11:16:07 +0300

    add configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:@tanstack/eslint-plugin-query/recommended',
+      'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+      'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+      ],
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
   },
-}
+};
