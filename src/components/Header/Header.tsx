e352b26cc7159370c130cf6135c7f045f73e1e52
Dmitry <mityourik@gmail.com>
2024-06-17 19:16:06 +0300

    add logo and nav container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,22 +16,22 @@ const Header: React.FC = () => {
                 </div>
                 <Search />
             </div>
-
         </div>
-      <div className={styles['header__logo']}>
-        <img src="logo.png" alt="Первая школа ЭЭГ-техников" className={styles['header__logo-image']} />
-        <span className={styles['header__logo-text']}>Первая школа ЭЭГ-техников</span>
-      </div>
-      <nav className={styles['header__nav']}>
-        <ul className={styles['header__nav-list']}>
-          <li className={styles['header__nav-item']}><a href="#about" className={styles['header__nav-link']}>О школе</a></li>
-          <li className={styles['header__nav-item']}><a href="#training" className={styles['header__nav-link']}>Обучение</a></li>
-          <li className={styles['header__nav-item']}><a href="#information" className={styles['header__nav-link']}>Информации</a></li>
-          <li className={styles['header__nav-item']}><a href="#conference" className={styles['header__nav-link']}>Конференция</a></li>
-          <li className={styles['header__nav-item']}><a href="#clinical-work" className={styles['header__nav-link']}>Клиническая работа</a></li>
-          <li className={styles['header__nav-item']}><a href="#contacts" className={styles['header__nav-link']}>Контакты</a></li>
-        </ul>
-      </nav>
+        <div className={styles['header__logo-and-nav']}>
+            <div className={styles['header__logo']}>
+                <img src="logo.png" alt="Первая школа ЭЭГ-техников" className={styles['header__logo-image']} />
+            </div>
+            <nav className={styles['header__nav']}>
+                <ul className={styles['header__nav-list']}>
+                <li className={styles['header__nav-item']}><a href="#about" className={styles['header__nav-link']}>О школе</a></li>
+                <li className={styles['header__nav-item']}><a href="#training" className={styles['header__nav-link']}>Обучение</a></li>
+                <li className={styles['header__nav-item']}><a href="#information" className={styles['header__nav-link']}>Информации</a></li>
+                <li className={styles['header__nav-item']}><a href="#conference" className={styles['header__nav-link']}>Конференция</a></li>
+                <li className={styles['header__nav-item']}><a href="#clinical-work" className={styles['header__nav-link']}>Клиническая работа</a></li>
+                <li className={styles['header__nav-item']}><a href="#contacts" className={styles['header__nav-link']}>Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
   );
 };
